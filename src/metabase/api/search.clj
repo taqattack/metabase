79ad45036a94691fe23552d806c8344b9e24dad8
John Swanson <john.swanson@metabase.com>
2024-02-29 10:50:44 -0800

    enforce new permissions in search
    NOTE: I turned off some tests, which were making use of the
    `mt/with-all-users-data-perms-graph!`. Since the data perms graph
    functionality doesn't work with the new perms yet, I just disabled the
    tests. We'll need to re-enable them when we fix up the graph bits to
    allow setting the new perms.

--- src/metabase/api/search.clj
+++ src/metabase/api/search.clj
@@ -354,16 +354,21 @@
 (defmethod check-permissions-for-model :table
   [_archived instance]
   ;; we've already filtered out tables w/o collection permissions in the query itself.
-  (= :unrestricted (data-perms/table-permission-for-user api/*current-user-id*
-                                                         :perms/data-access
-                                                         (database/table-id->database-id (:id instance))
-                                                         (:id instance))))
+  (and (= :unrestricted (data-perms/database-permission-for-user api/*current-user-id*
+                                                                 :perms/view-data
+                                                                 (database/table-id->database-id (:id instance))))
+       (contains? #{:query-builder :query-builder-and-native}
+                  (data-perms/table-permission-for-user api/*current-user-id*
+                                                        :perms/create-queries
+                                                        (database/table-id->database-id (:id instance))
+                                                        (:id instance)))))
 
 (defmethod check-permissions-for-model :indexed-entity
   [_archived? instance]
   (and
-   (= :yes (data-perms/database-permission-for-user api/*current-user-id* :perms/native-query-editing (:database_id instance)))
-   (= :unrestricted (data-perms/full-db-permission-for-user api/*current-user-id* :perms/data-access (:database_id instance)))))
+   (= :unrestricted (data-perms/database-permission-for-user api/*current-user-id* :perms/view-data (:database_id instance)))
+   (= :query-builder-and-native
+      (data-perms/full-db-permission-for-user api/*current-user-id* :perms/create-queries (:database_id instance)))))
 
 (defmethod check-permissions-for-model :metric
   [archived? instance]
