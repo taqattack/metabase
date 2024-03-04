1bf21321d3218fa5f61715b9ba14c652a19d6ed5
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-03-04 16:14:58 -0500

    wip

--- frontend/src/metabase/admin/performance/components/DatabaseStrategyEditor.tsx
+++ frontend/src/metabase/admin/performance/components/DatabaseStrategyEditor.tsx
@@ -1,8 +1,22 @@
 import type { Dispatch, MouseEvent, SetStateAction } from "react";
-import { useEffect, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { t } from "ttag";
-
+import _ from "underscore";
+import { useAsync } from "react-use";
+import type Database from "metabase-lib/metadata/Database";
+import { useDatabaseListQuery } from "metabase/common/hooks";
+import LoadingAndErrorWrapper from "metabase/components/LoadingAndErrorWrapper";
 import { color } from "metabase/lib/colors";
+import { useDispatch } from "metabase/lib/redux";
+import { addUndo } from "metabase/redux/undo";
+import { CacheConfigApi } from "metabase/services";
 import {
   Button,
   Flex,
@@ -13,15 +27,14 @@ import {
   Text,
   Title,
 } from "metabase/ui";
-import type Database from "metabase-lib/metadata/Database";
-
 import type {
+  Config,
   DBStrategySetter,
   DefaultMappings,
   DefaultsMap,
   DurationStrategy,
   GetConfigByModelId,
-  RootStrategySetter,
+  Model,
   Strategy,
   StrategyType,
   TTLStrategy,
@@ -36,7 +49,6 @@ import {
   durationStrategyValidationSchema,
   ttlStrategyValidationSchema,
 } from "../validation";
-
 import {
   ConfigureSelectedStrategy,
   PositiveNumberInput,
@@ -48,19 +60,242 @@ import {
   TabWrapper,
 } from "./DatabaseStrategyEditor.styled";
 
+const defaultRootStrategy: Strategy = { type: "nocache" };
+
 export const DatabaseStrategyEditor = ({
-  databases,
-  dbConfigs,
-  setRootStrategy,
-  setDBStrategy,
-  clearDBOverrides,
+  tabsRef,
+  setTabsHeight,
 }: {
-  databases: Database[];
-  dbConfigs: GetConfigByModelId;
-  setDBStrategy: DBStrategySetter;
-  setRootStrategy: RootStrategySetter;
-  clearDBOverrides: () => void;
+  tabsRef: React.RefObject<HTMLDivElement>;
+  setTabsHeight: (height: number) => void;
 }) => {
+  const {
+    data: databases = [],
+    error: errorWhenLoadingDatabases,
+    isLoading: areDatabasesLoading,
+  } = useDatabaseListQuery();
+
+  const {
+    value: configsFromAPI,
+    loading: areConfigsLoading,
+    error: errorWhenLoadingConfigs,
+  }: {
+    value?: unknown[];
+    loading: boolean;
+    error?: any;
+  } = useAsync(async () => {
+    const [rootConfigsFromAPI, dbConfigsFromAPI] = await Promise.all([
+      CacheConfigApi.list({ model: "root" }),
+      CacheConfigApi.list({ model: "database" }),
+    ]);
+    const configs = [
+      ...(rootConfigsFromAPI?.items ?? []),
+      ...(dbConfigsFromAPI?.items ?? []),
+    ];
+    return configs;
+  }, []);
+
+  const [configs, setConfigs] = useState<Config[]>([]);
+
+  const dispatch = useDispatch();
+
+  const showSuccessToast = useCallback(async () => {
+    dispatch(
+      addUndo({
+        message: "Updated",
+        toastColor: "success",
+        dismissButtonColor: color("white"),
+      }),
+    );
+  }, [dispatch]);
+
+  const showErrorToast = useCallback(async () => {
+    dispatch(
+      addUndo({
+        icon: "warning",
+        message: "Error",
+        toastColor: "error",
+        dismissButtonColor: color("white"),
+      }),
+    );
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (configsFromAPI) {
+      setConfigs(configsFromAPI as Config[]);
+    }
+  }, [configsFromAPI]);
+
+  const dbConfigs: GetConfigByModelId = useMemo(() => {
+    const map: GetConfigByModelId = new Map();
+    configs.forEach(config => {
+      map.set(config.model === "database" ? config.model_id : "root", config);
+    });
+    if (!map.has("root")) {
+      map.set("root", {
+        model: "root",
+        model_id: 0,
+        strategy: defaultRootStrategy,
+      });
+    }
+    return map;
+  }, [configs]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedRequest = useCallback(
+    _.debounce(
+      (
+        requestFunction: (
+          arg: any,
+          options: { fetch?: boolean },
+        ) => Promise<any>,
+        arg: any,
+        options: { fetch?: boolean; [key: string]: any },
+        onSuccess: () => Promise<any>,
+        onError: () => Promise<any>,
+      ) => {
+        options.fetch ??= true;
+        return requestFunction(arg, options).then(onSuccess).catch(onError);
+      },
+      // TODO: Perhaps increase the debounce wait time when user is
+      // using arrow keys to change the strategy type
+      200,
+    ),
+    [],
+  );
+
+  const setStrategy = useCallback(
+    (model: Model, model_id: number, newStrategy: Strategy | null) => {
+      const baseConfig: Pick<Config, "model" | "model_id"> = {
+        model,
+        model_id,
+      };
+      const otherConfigs = configs.filter(
+        config => config.model_id !== model_id,
+      );
+
+      const oldConfig = dbConfigs.get(model_id);
+      const onSuccess = async () => {
+        await showSuccessToast();
+      };
+      const onError = async () => {
+        await showErrorToast();
+        // Revert to earlier state
+        setConfigs(oldConfig ? [...otherConfigs, oldConfig] : otherConfigs);
+        // FIXME: this reverts to an earlier state even if the user has already
+        // changed the value again. We should revert only if there is no newer
+        // change
+      };
+
+      if (newStrategy) {
+        const newConfig: Config = {
+          ...baseConfig,
+          strategy: newStrategy,
+        };
+        setConfigs([...otherConfigs, newConfig]);
+        debouncedRequest(
+          CacheConfigApi.update,
+          newConfig,
+          {},
+          onSuccess,
+          onError,
+        );
+      } else {
+        setConfigs(otherConfigs);
+        debouncedRequest(
+          CacheConfigApi.delete,
+          baseConfig,
+          { hasBody: true },
+          onSuccess,
+          onError,
+        );
+      }
+    },
+    [configs, dbConfigs, debouncedRequest, showErrorToast, showSuccessToast],
+  );
+
+  const setRootStrategy = (strategy: Strategy) =>
+    setStrategy("root", 0, strategy);
+  const setDBStrategy = (databaseId: number, strategy: Strategy) =>
+    setStrategy("database", databaseId, strategy);
+
+  const clearDBOverrides = useCallback(() => {
+    setConfigs(configs => configs.filter(({ model }) => model !== "database"));
+
+    configs
+      .filter(({ model }) => model === "database")
+      .forEach(config => {
+        if (config.model !== "database") {
+          return;
+        }
+        const onSuccess = async () => {
+          await showSuccessToast();
+        };
+        const onError = async () => {
+          await showErrorToast();
+          // TODO: Revert to earlier state?
+        };
+        debouncedRequest(
+          CacheConfigApi.delete,
+          config,
+          { hasBody: true },
+          onSuccess,
+          onError,
+        );
+      });
+  }, [configs, debouncedRequest, showErrorToast, showSuccessToast]);
+
+  const [showLoadingSpinner, setShowLoadingSpinner] = useState(false);
+
+  // TODO: If this doesn't need to depend on areDatabasesLoading etc then move it up
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const tabs = tabsRef.current;
+      if (!tabs) {
+        return;
+      }
+      const tabsElementTop = tabs.getBoundingClientRect().top;
+      const newHeight = window.innerHeight - tabsElementTop - tabs.clientTop;
+      setTabsHeight(newHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    setTimeout(handleResize, 50);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [tabsRef, areDatabasesLoading, areConfigsLoading, areConfigsLoading]);
+
+  useEffect(
+    /**
+     * @see https://metaboat.slack.com/archives/C02H619CJ8K/p1709558533499399
+     */
+    function delayLoadingSpinner() {
+      setTimeout(() => {
+        setShowLoadingSpinner(true);
+      }, 300);
+    },
+    [],
+  );
+
+  if (errorWhenLoadingConfigs || areConfigsLoading) {
+    return showLoadingSpinner ? (
+      <LoadingAndErrorWrapper
+        error={errorWhenLoadingConfigs}
+        loading={areConfigsLoading}
+      />
+    ) : null;
+  }
+
+  if (errorWhenLoadingDatabases || areDatabasesLoading) {
+    return showLoadingSpinner ? (
+      <LoadingAndErrorWrapper
+        error={errorWhenLoadingDatabases}
+        loading={areDatabasesLoading}
+      />
+    ) : null;
+  }
+
   /** Id of the database currently being edited, or 'root' for the root strategy */
   const [currentId, setCurrentId] = useState<number | "root" | null>(null);
   const rootStrategy = dbConfigs.get("root")?.strategy;
